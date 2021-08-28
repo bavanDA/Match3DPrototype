cfc80f9208b87bb23f5078967fbef9c241080e92
mr3 <mr3@gmail.com>
2021-08-28 04:49:00 +0430

    Dragabe Objects Added

---
+++ Match3DPrototype/Assets/Scripts/playableObjects.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class playableObjects : MonoBehaviour
+{
+    public int id;
+    Vector3 mousePosition;
+    Vector3 mouseOffset;
+    float zeroPoint;
+    private void OnMouseDown()
+    {
+        transform.Translate(0f, 1f, 0f);
+        this.gameObject.GetComponent<Rigidbody>().useGravity = false;
+        zeroPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        mouseOffset = gameObject.transform.position - getMouseWorldPos();
+    }
+    private void OnMouseUp()
+    {
+        this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = getMouseWorldPos() + mouseOffset;
+    }
+
+    Vector3 getMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = zeroPoint;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
