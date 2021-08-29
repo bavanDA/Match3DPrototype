ff0229af248a227cfdd9dc3bbbb8de986d2c5022
mr3 <mr3@gmail.com>
2021-08-29 20:51:30 +0430

    trash ok!!!

---
+++ Match3DPrototype/Assets/Scripts/SimlarObjectsCheck.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimlarObjectsCheck : MonoBehaviour
+{
+    public GameObject activeObject1;
+    public GameObject activeObject2;
+    public GameObject pos1;
+    public GameObject pos2;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (activeObject1.gameObject.tag!="trash")
+        {
+            
+            if (activeObject2.gameObject.tag != "trash")
+            {
+                if (activeObject1.GetComponent<playableObjects>().id == activeObject2.GetComponent<playableObjects>().id)
+                {
+                    Destroy(activeObject1);
+                    Destroy(activeObject2);
+                    Debug.Log("similar");
+                    activeObject1 = pos1;
+                    activeObject2 = pos2;
+                    Debug.Log(activeObject1.tag);
+                    Debug.Log(activeObject2.tag);
+                }
+            }
+        }
+    }
+}
--- Match3DPrototype/Assets/Scripts/playableObjects.cs
+++ Match3DPrototype/Assets/Scripts/playableObjects.cs
@@ -9,8 +9,59 @@ public class playableObjects : MonoBehaviour
     Vector3 mousePosition;
     Vector3 mouseOffset;
     float zeroPoint;
+    public bool Drag;
+    public GameObject trash;
+    public GameObject targetPosition1;
+    public GameObject targetPosition2;
+    public bool isAct;
+    private void Start()
+    {
+        Drag = false;
+        isAct = false;
+    }
+
+    private void Update()
+    {
+        if (Drag == false && !isAct)
+        {
+            if (transform.position.x > (trash.transform.position.x - 2) && transform.position.x < (trash.transform.position.x + 2))
+            {
+                if(transform.position.z > trash.transform.position.z - 1 && transform.position.z < trash.transform.position.z + 1)
+                {
+                    if (trash.GetComponent<SimlarObjectsCheck>().activeObject1.tag=="trash")
+                    {
+                        trash.GetComponent<SimlarObjectsCheck>().activeObject1 = this.gameObject;
+                        
+                        isAct = true;
+                    }
+                    else
+                    {
+                        trash.GetComponent<SimlarObjectsCheck>().activeObject2 = this.gameObject;
+                        this.transform.position = targetPosition2.transform.position;
+                    }
+                }
+            }
+        }
+
+        if (isAct)
+        {
+            this.transform.position = targetPosition1.transform.position;
+            Rigidbody rb= this.GetComponent<Rigidbody>();
+            rb.drag = 500;
+            this.transform.rotation = new Quaternion(0f, 0f, 0f,0f);
+            //Debug.Log("1:" + trash.GetComponent<SimlarObjectsCheck>().activeObject1.tag);
+            //Debug.Log("2:" + trash.GetComponent<SimlarObjectsCheck>().activeObject2.tag);
+        }
+        else
+        {
+            Rigidbody rb = this.GetComponent<Rigidbody>();
+            rb.drag = 0.5f;
+        }
+    }
     private void OnMouseDown()
     {
+        Drag = true;
+        isAct = false;
         transform.Translate(0f, 1f, 0f);
         this.gameObject.GetComponent<Rigidbody>().useGravity = false;
         zeroPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
@@ -19,6 +70,7 @@ public class playableObjects : MonoBehaviour
     private void OnMouseUp()
     {
         this.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        Drag = false;
     }
 
     private void OnMouseDrag()
